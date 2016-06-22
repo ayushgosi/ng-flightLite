b870ddd3943d92afba232c6eef8390c3eace49e3
ayushgosi <anime.ayush@gmail.com>
2016-06-22 14:22:59 +0530

    Added a third page to display flights

--- js/app.js
+++ js/app.js
@@ -15,19 +15,89 @@ flightLite.config(function($routeProvider){
     controller: 'searchController',
     controllerAs: 'search'
   })
+  .when('/flights', {
+    templateUrl: 'pages/flights.html',
+    controller: 'flightsController',
+    controllerAs: 'flights'
+  })
 
 });
 
 //Services
-flightLite.service('flightService', function($resource){
+flightLite.service('iataService', function($resource){
+  var res = $resource('js/iataCodesUSA.json');
+  return res.query();
+});
+
+flightLite.service('flightService', function($resource, $q){
   var vm = this;
 
-  //IATA json call
-  vm.iata = $resource('js/iataCodesUSA.json');
+  vm.in = {};
 
-  //flightsdata
-  vm.flightData = $resource('http://terminal2.expedia.com/x/mflights/search?departureAirport=LAX&arrivalAirport=ORD&departureDate=2016-06-22&childTravelerAge=2&apikey=KmshkuaH90MoSVdDXnHMweROagV4lIAQ');
+  vm.getData = function(){
+    var res = $resource('http://terminal2.expedia.com/x/mflights/search?apikey=KmshkuaH90MoSVdDXnHMweROagV4lIAQ&prettyPrint=true&maxOfferCount=8');
+    var resData = res.get({departureDate:vm.in.date, departureAirport:vm.in.depCode, arrivalAirport:vm.in.desCode});
 
+    var deferred = $q.defer();
+    resData.$promise.then(function(result){
+
+      var l = result.legs.length;
+
+      var resultArr = [];
+
+      for(i = 0; i<l; i++){
+        var obj = {};
+
+        obj['departureCity'] = result.searchCities[0].city;
+        obj['departureCityCode'] = result.searchCities[0].code;
+        obj['departureCityProvince'] = result.searchCities[0].province;
+        obj['destinationCity'] = result.searchCities[1].city;
+        obj['destinationCityCode'] = result.searchCities[1].code;
+        obj['destinationCityProvince'] = result.searchCities[1].province;
+        obj['totalFare'] = result.offers[i].totalFarePrice.formattedWholePrice;
+
+        var len = result.legs[i].segments.length;
+        obj['stops'] = len - 1;
+        obj['departureTime'] = result.legs[i].segments[0].departureTime;
+
+        for(j=0; j<len; j++){
+          var count = j+1;
+          obj['airlineName'+count] = result.legs[i].segments[j].airlineName;
+          obj['airlineNameCode'+count] = result.legs[i].segments[j].airlineCode;
+          obj['departureCity'+count] = result.legs[i].segments[j].departureAirportAddress.city;
+          obj['departureCityState'+count] = result.legs[i].segments[j].departureAirportAddress.state;
+          obj['departureCityCode'+count] = result.legs[i].segments[j].departureAirportCode;
+          obj['destinationCity'+count] = result.legs[i].segments[j].arrivalAirportAddress.city;
+          obj['destinationCityState'+count] = result.legs[i].segments[j].arrivalAirportAddress.state;
+          obj['destinationCityCode'+count] = result.legs[i].segments[j].arrivalAirportCode;
+          obj['distance'+count] = result.legs[i].segments[j].distance +" "+ result.legs[i].segments[j].distanceUnits;
+          obj['departureTime'+count] = result.legs[i].segments[j].departureTime;
+          obj['destinationTime'+count] = result.legs[i].segments[j].arrivalTime;
+          obj['flightNumber'+count] = result.legs[i].segments[j].flightNumber;
+          obj['duration'+count] = result.legs[i].segments[j].duration;
+
+          var layoverTime = result.legs[i].segments[j].groundTime;
+          if(layoverTime){
+            obj['layoverTime'+count] = layoverTime;
+          }
+          // else{
+          //   obj['layoverTime'+count] = '';
+          // }
+
+          obj['destinationTime'] = result.legs[i].segments[j].arrivalTime;
+        }
+
+        resultArr.push(obj);
+      }
+
+      deferred.resolve(resultArr);
+    }, function(err){
+      console.error('Error');
+    });
+    return deferred.promise;
+  };
+
+  // vm.getFlightsData = null;
 });
 
 
@@ -43,53 +113,56 @@ flightLite.controller('mainController',['$scope', '$firebaseArray', '$resource',
 
 }]);
 
-flightLite.controller('searchController',['$scope', '$timeout', '$q', '$log', '$firebaseArray', '$resource', 'flightService', function($scope, $timeout, $q, $log, $firebaseArray, $resource, flightService){
+flightLite.controller('searchController',['$scope', 'iataService', 'flightService', function($scope, iataService, flightService){
   var vm = this;
+
+  //IATA json call
+  vm.iataData = iataService;
+  console.log(vm.iataData);
 
   //Date Picker
   vm.myDate = new Date();
-  vm.minDate = new Date(
-    vm.myDate.getFullYear(),
-    vm.myDate.getMonth(),
-    vm.myDate.getDate());
-    vm.maxDate = new Date(
-      vm.myDate.getFullYear(),
-      vm.myDate.getMonth() + 2,
-      vm.myDate.getDate());
+  vm.minDate = new Date(vm.myDate.getFullYear(), vm.myDate.getMonth(), vm.myDate.getDate());
+  vm.maxDate = new Date(vm.myDate.getFullYear(), vm.myDate.getMonth() + 2, vm.myDate.getDate());
+  //inputDate
+  vm.month = vm.myDate.getMonth()+1;
+  vm.selectedDate = vm.myDate.getFullYear() + "-" + vm.month + "-" + vm.myDate.getDate();
+  console.log(vm.selectedDate);
 
-      //IATA json call
-      vm.iataCodes = flightService.iata.query();
-      console.log(vm.iataCodes);
+  //search input
+  vm.departureObj = null;
+  vm.destinationObj = null;
 
-      //search input
-      vm.dropdownObjects = vm.iataCodes;
-      vm.departureObj = vm.dropdownObjects[0];
-      vm.destinationObj = vm.dropdownObjects[1];
+  vm.sendInputDetails = function(){
+    console.info("Inside sendInputDetails");
+    flightService.in = {
+      date: vm.selectedDate,
+      depCode: vm.departureObj.iatacode,
+      desCode: vm.destinationObj.iatacode
+    };
+  };
+}]);
 
-      vm.adultTravelers = null;
-      vm.childTravelersAge = null;
 
-      //flightsdata
-      vm.flightData = function(){
-        return flightService.flightData.get({
-          departureDate: vm.myDate,
-          returnDate: null,
-          departureAirport: vm.departureObj.iatacode,
-          arrivalAirport: vm.destinationObj.iatacode,
-          prettyPrint: true,
-          numberOfAdultTravelers: null,
-          childTravelerAge: null,
-          maxOfferCount: 10
-        })
-        console.log(vm.flightData);
-      };
-      }]);
+flightLite.controller('flightsController',['$scope', 'iataService', 'flightService', function($scope, iataService, flightService){
+  var vm = this;
 
-      // flightLite.controller('mainController',['$scope', '$firebaseArray', '$resource', function($scope, $firebaseArray, $resource){
-      //   var vm = this;
-      //
-      //   var ref = new Firebase('https://cricbase626262.firebaseio.com/Matches');
-      //   vm.matches = $firebaseArray(ref);
-      //
-      //
-      // }]);
+  var data = flightService.getData();
+  data.then(function(resData){
+    vm.flightsData = resData;
+    console.log(vm.flightsData);
+  }, function(err){
+    console.error("error!");
+  });
+
+}]);
+
+
+// flightLite.controller('mainController',['$scope', '$firebaseArray', '$resource', function($scope, $firebaseArray, $resource){
+//   var vm = this;
+//
+//   var ref = new Firebase('https://cricbase626262.firebaseio.com/Matches');
+//   vm.matches = $firebaseArray(ref);
+//
+//
+// }]);
